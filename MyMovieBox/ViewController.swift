0aef382e437cc553aec624a0b0fe57d9ac79e27b
norang <norang1221@gmail.com>
2025-01-24 17:58:50 +0900

    스플래시 화면 구현 - 레이아웃 구성 및 에셋 배치 - 스플래시 화면 2초간 유지

--- MyMovieBox/ViewController.swift
+++ MyMovieBox/ViewController.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
-
+import SnapKit
 class ViewController: UIViewController {
 
+    let label = {
+        let label = UILabel()
+        label.text = "on boarding or main?"
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        print(#function)
+        
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
     }
 
 
